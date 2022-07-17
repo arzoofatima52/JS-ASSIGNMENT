5a1060a9123965274041379a9e7fff1b39a8a858
arzoofatima52 <arzoofatima025@gmail.com>
2022-07-17 10:33:21 +0500

    commit

---
+++ app.js
@@ -0,0 +1,447 @@
+//-------------------------------CHAPTER -1--------------------------------------
+
+//  ----------Question-1----------
+// ("Welcome to the ABC Websialertte");
+
+//  ----------Question-2----------
+// alert("Error! Please Enter a valid Password.")
+
+// ----------Question-3----------
+// alert("Welcome to JS Land...\nHappy Coding!") ;
+
+// ----------Question-4----------
+// alert("Welcome to JS Land...");
+// alert("Happy Coding!" ) 
+
+// ----------Question-5----------
+// I've done it by inpecting webpage going to console and type in 
+// alert("Hello! I can run JS through my web browser's console"); and press enter.
+
+//----------Question-6----------
+// var a = 5;
+// var b = 6;
+// var c = a + b ;
+//// alert(c);
+
+// ----------Question-7----------
+//// ----Done In Index.html file-----
+
+
+
+//-------------------------------CHAPTER -2--------------------------------------
+// ----------Question-1-----------
+// var username;
+
+// ----------Question-2-----------
+// var myname;
+// myname = "ARZOO FATIMA"
+
+// ----------Question-3-----------
+// var message;
+// message = "Hello World";
+// alert(message);
+
+// ----------Question-4-----------
+// var stdname = "Jhone Doe";
+// var age = 15;
+// var course = "Certified Mobile Application Development";
+// alert(stdname);
+// alert (age+ "years old");
+// alert(course);
+
+// ----------Question-5-----------
+// var a ;
+// a = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(a);
+
+// ----------Question-6-----------
+// var email = "arzoofatima@abc.com";
+// alert("My email address is " + email);
+
+// ----------Question-7-----------
+// var book;
+// book = "A smarter way to learn JavaScript";
+// alert("I am trying to learn from the Book " + book);
+
+// ----------Question-8-----------
+// document.write("Yah! I can write HTML content through JavaScript");
+
+// ----------Question-9-----------
+// var h ;
+// h = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(h);
+
+
+//-------------------------------CHAPTER -3----------------------------------------
+// ----------Question-1-----------
+// age = 2;
+// alert("I am " + age + " years old");
+
+// ----------Question-2-----------
+// var N = 14;
+// alert("You have visited this site" +N+ " times");
+
+// ----------Question-3-----------
+// var birthyear = 2005;
+// document.write("My birthyear is " + birthyear ,  "<br>" );
+// document.write("Data type of my declared variable is numbber")
+
+
+// // ----------Question-3-----------
+// var visitorname = "John Doe";
+// var producttitle = "T-shirt(s)";
+// var quantity = 5;
+// document.write(visitorname + " ordered " + quantity + " "+producttitle +" on XYZ Clothing store.");
+
+//-------------------------------CHAPTER -4----------------------------------------
+
+// // ----------Question-1-----------
+// var a,b,c;
+// // ----------Question-2-----------
+
+//  -----------legal variable ----------
+// var name            
+// var Name            
+// var $name            
+// var n12124           
+// var _name  
+
+// ---------------- illegal variable -----------
+// var 1name            
+// var -Name            
+// var (name            
+// var ,12124           
+// var \name
+
+// // ----------Question-3-----------
+
+//    document.write("Rules for Naming JS variables");
+// document.write("<br>");
+// document.write("<br>");
+// document.write("<br>");
+
+// document.write("Variable names can only contain Numbers ,$ sign ,_ and Integers after a alphabets");
+// document.write("<br>");
+
+// document.write("Variables must begin with a Small Alphabets ,$ sign ,_ sign and big letters");
+// document.write("<br>");
+
+// document.write("Variable names are case sensitive");
+// document.write("<br>");
+// document.write(" Variable names should not be JS Keywords");
+
+
+
+
+// ---------------CHAP-5---------
+
+// ----------Question-1-----------
+// var a = 3 ;
+// var b = 5 ;
+// var c = a + b
+// document.write("Sum of " + a +" and "+ b+ " is " +c)
+
+// ----------Question-2-----------
+// var a = 3 ;
+// var b = 5 ;
+// var c = a - b
+// document.write("Subtration of " + a +" and "+ b+ " is " +c)
+
+
+// var a = 3 ;
+// var b = 5 ;
+// var c = a * b
+// document.write("Multiply of " + a +" and "+ b+ " is " +c)
+
+
+// var a = 3 ;
+// var b = 5 ;
+// var c = a / b
+// document.write("Division of " + a +" and "+ b+ " is " +c)
+
+
+// var a = 3 ;
+// var b = 5 ;
+// var c = a % b
+// document.write("Mod of " + a +" and "+ b+ " is " +c)
+
+// ----------Question-3------------
+//  var a ;
+//  document.write("Value after variable declaration is " + a);
+//  var a = 5;
+//  document.write("<br>")
+//  document.write("Initial value = " + a);
+//  document.write("<br>")
+//  var b = 1;
+//  var c = a+b;
+//  document.write("Value after increment is = " + c);
+//  document.write("<br>")
+//  var d = 7 ;
+//  var e = c+d;
+//  document.write("Value after addition is = " + e);
+//  document.write("<br>")
+// var f = e - 1;
+// document.write("Value after decrement is = " + f);
+// document.write("<br>")
+// var g = f % 3;
+// document.write("The remainder is = " + g);
+
+// ----------Question-4------------
+// var a = 600;
+// var b = 600 * 5;
+// document.write("Total cost to buy 5 tickets to a movie is " + b + " PKR")
+
+// ----------Question-5------------
+// var Table_4 = ("Table of 4")
+// document.write(Table_4)
+// document.write("<br>")
+// var four = 4
+// var four1 = ("4*1=")
+// var fourone = four*1
+// document.write(four1+fourone)
+// document.write("<br>")
+
+// var four2 = ("4*2=")
+// var fourtwo = four*2
+// document.write(four2+fourtwo)
+// document.write("<br>")
+
+// var four3 = ("4*3=")
+// var fourthree = four*3
+// document.write(four3+fourthree)
+// document.write("<br>")
+
+// var four4 = ("4*4=")
+// var fourfour = four*4
+// document.write(four4+fourfour)
+// document.write("<br>")
+
+// var four5 = ("4*5=")
+// var fourfive = four*5
+// document.write(four5+fourfive)
+// document.write("<br>")
+
+// var four6 = ("4*6=")
+// var foursix = four*6
+// document.write(four6+foursix)
+// document.write("<br>")
+
+// var four7 = ("4*7=")
+// var fourseven = four*7
+// document.write(four7+fourseven)
+// document.write("<br>")
+
+// var four8 = ("4*8=")
+// var foureight = four*8
+// document.write(four8+foureight)
+// document.write("<br>")
+
+// var four9 = ("4*9=")
+// var fournine = four*9
+// document.write(four9+fournine)
+// document.write("<br>")
+
+// var four10 = ("4*10=")
+// var fourten = four*10
+// document.write(four10+fourten)
+// document.write("<br>")
+ 
+ // ----------Question-6------------
+
+ //  var C = 10
+//  var cinotf =(C * 9/5) +32
+
+// document .write(C+"°C"+"is"+cinotf+"°F")
+// document.write("<br>")
+
+//  var F = 10
+//  var finotfc =(F- 32) *5/9
+//  document .write(F+"°F"+"is"+finotfc+"°C")
+
+
+ // ----------Question-7------------
+// var  Priceofitem1 = 102
+// var  Priceofitem2 = 1050
+// var  Quantityofitem1 = 2
+// var  Quantityofitem2 = 10                               
+// var  shippingcharges = 200
+
+// document.write("Price of item 1 is:"+Priceofitem1)
+// document.write("<br>")
+// document.write("Quantity of item 1 is:"+Quantityofitem1)
+// document.write("<br>")
+// document.write("Price of item 2 is:"+Priceofitem2)
+// document.write("<br>")
+// document.write("Quantity of item 2 is:"+Quantityofitem2)
+// document.write("<br>")
+// document.write("Shipping Charges:"+shippingcharges)
+// document.write("<br>")
+// document.write("Toatl cost of your order is"+(Priceofitem1*Quantityofitem1+Priceofitem2*Quantityofitem2+shippingcharges))
+
+
+  // ----------Question-8------------
+// var totalmarks = 100
+// var obtainmarks = 79
+// var percentage = obtainmarks/totalmarks*100
+// document.write("<h1>MARK SHEET</h1>")
+// document.write("Total Marks:100")
+// document.write("<br>")
+// document.write("Obatin Marks:79")
+// document.write("<br>")
+// document.write("Percentage:"+percentage+"%")
+
+ // ----------Question-9------------
+// var dollar = 104.80 
+// var saudi = 28  
+// var total$ = dollar*10
+// var totals = saudi*25
+// var inpkr = total$+totals
+// document.write("<h1>CURRENCY IN PKR</h1>")
+// document.write("Total currency in PKR:"+inpkr)
+
+ // ----------Question-10------------
+
+// var no = 7+5*10/2
+// document.write(no)
+
+// ----------Question-11------------
+// document.write("<h1>AGE CALCULATOR</h1>")
+// var currentyear = 2022
+// var birthdayyear = 2004
+// var age = currentyear-birthdayyear
+// document.write("Current Year:"+currentyear)
+// document.write("<br>")
+// document.write("Birth Year:"+birthdayyear)
+// document.write("<br>")
+// document.write("Your age is :"+age)
+
+// ----------Question-12------------
+// var radius = 20
+// var circumferance = radius*2*3.14
+// document.write("<h1>The Geometizer</h1>")
+// document.write("The Radius of circle is:"+radius)
+// document.write("<br>")
+// document.write("The Circumferance of circle is:"+circumferance)
+// var area = 3.14*20*20
+// document.write("<br>")
+// document.write("The area of circle is:"+area)
+
+// // ----------Question-13------------
+// var snack = "chocolate chip";
+// var age = 15;
+// var eage = 65;
+// var amt = 3;
+// var cal = (eage - age) * amt
+// document.write("Favourite snack: " + snack);
+//  document.write("<br>")
+// document.write("Current Age: " + age);
+// document.write("<br>")
+// document.write("Estimated Maximum age " + eage);
+// document.write("<br>")
+// document.write("Amount of snacks per day: " + amt);
+// document.write("<br>")
+// document.write("Favourite snack: " + snack);
+// document.write("<br>")
+// document.write("You will need "+ cal +" "+ snack +" to last you until the ripe old age of " + eage);
+
+
+// document.write("Qualifications:  <br><br>")
+
+// var arraylist = ["SSC","HSC","BCS","BS","BCOM","MS","M.Phil.","Phd"];
+
+// for (var i=0; i<arraylist.length; i++)
+// {
+//   document.write(arraylist[i] +"<br>");
+// }
+
+
+// / ----------DATA METHODS-----------
+
+
+// var a = new Date();
+// document.write(a);
+
+
+// var  months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+// var a = new Date();
+// var cmonth = a.getMonth();
+// var m = months[cmonth]
+// document.write( "Current Month : " + m );
+
+
+// var day = ["MON", "TUES","WED","THURS", "FRI", "SAT", "SUN"]
+// var a = new Date();
+// var today = a.getDay();
+// var t = day[today];
+// document.write("Today is: " +t[0],t[1],t[3])
+
+
+// var day = ["MON", "TUES","WED","THURS", "FRI", "SAT", "SUN"]
+// var a = new Date();
+// var today = a.getDay();
+// var t = day[today];
+// if( t=="SUN" || t=="SAT" )
+// {
+//     document.write("It's a Fun day")
+// }
+// else{
+//     document.write("It's not a Fun day")
+// }
+
+
+// var a = new Date();
+// var date = a.getDate();
+// if(date <= 15 ){
+// document.write("First fifteen of the month")
+// }
+// else{
+//     document.write("Last days of the month")
+// }
+
+
+// var td = new Date();
+// document.write("Current Date: " + td);
+// var tt = td.getTime();
+// document.write("<br> Elapsed milliseconds since midnight, Jan 1, 1970: " + tt);
+// var td = td.getMinutes();
+// var d= tt-td;
+// var dm = d/(1000*60*60)
+// var amin = Math.floor(dm);
+// document.write("<br> Elapsed minutes since midnight, Jan 1, 1970: " + amin);
+
+
+// var time = new Date()
+// var hours = time.getHours
+// if( hours<12){
+//     document.write("It's AM")}
+// else{
+//     document.write("It's PM")}
+
+
+// var a = new Date("June 30, 2022")
+// document.write(a)
+
+
+
+// ------------31-38-------------
+
+// function curentdate(){
+//     var time = new Date()
+//     alert(time)
+// }
+//  curentdate()
+
+
+//  function name(first,last)
+// {
+//  alert("Welcome " + first +last)
+
+// }
+// name(prompt("Enter your first name : "), prompt("Enter your last name : "))
+
+// function Add(a,b)
+
+
+
+
+
